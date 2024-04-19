17e222dfc7a5f3574db74ba0faa6a2ded425a291
Chameleon <developer@deltawarp.com>
2024-04-19 13:36:17 -0400

    removing select * broke edit

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -79,10 +79,6 @@ public class UserService : IUserService
         // copy model props to user
         _mapper.Map(model, user);
 
-        // make sure the user has the right id
-        user.Id = id;
-
-
         // save user
         await _userRepository.Update(user);
     }
